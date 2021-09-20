c4b953cab13eb4a310c09730bca82d11a4eb57e1
esantiago133 <edgardo.zeno@hotmail.com>
2021-09-20 01:26:17 -0400

    Cleanup code

--- HElloWorld/main.py
+++ HElloWorld/main.py
@@ -7,7 +7,6 @@ a = 5
 print(a)
 
 ed_turtle = turtle.Turtle()
-ed_turtle2 = turtle.Turtle()
 
 
 def square():
@@ -19,17 +18,12 @@ def square():
     ed_turtle.right(90)
     ed_turtle.forward(100)
 
-    ed_turtle2.forward(100)
-    ed_turtle2.left(90)
-    ed_turtle2.forward(100)
-    ed_turtle2.left(90)
-    ed_turtle2.forward(100)
-    ed_turtle2.left(90)
-    ed_turtle2.forward(100)
+    
 
 
 square()
-ed_turtle.forward(400)
-ed_turtle2.forward(400)
-ed_turtle.left(300)
+ed_turtle.forward(100)
+square()
+ed_turtle.left(90)
 ed_turtle.back(200)
+square()
